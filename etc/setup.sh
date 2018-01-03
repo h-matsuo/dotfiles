76dea4b44bfc503d25be508c0719e9eb1f06b45f
Hiroyuki Matsuo <h-matsuo@ist.osaka-u.ac.jp>
2018-01-03 12:26:26 +0900

    [update] change not to overwrite existing .bashrc

--- etc/setup.sh
+++ etc/setup.sh
@@ -8,32 +8,26 @@ mv git-completion.bash "${HOME}/.git-completion.bash"
 
 # Link .gitconfig
 if [ -f "${HOME}/.gitconfig" ]; then
-  rm "${HOME}/.gitconfig"
+  mv "${HOME}/.gitconfig"{,.org}
 fi
 ln -s "${DOTFILES_DIR}/.gitconfig" "${HOME}/.gitconfig"
 
+# Link .bash_profile
+if [ -f "${HOME}/.bash_profile" ]; then
+  mv "${HOME}/.bash_profile"{,.org}
+fi
+ln -s "${DOTFILES_DIR}/.bash_profile" "${HOME}/.bash_profile"
+
 # Link .bashrc
 if [ -f "${HOME}/.bashrc" ]; then
-  rm "${HOME}/.bashrc"
+  mv "${HOME}/.bashrc"{,.org}
 fi
 ln -s "${DOTFILES_DIR}/.bashrc" "${HOME}/.bashrc"
 
 # macOS only
 if [ "$(uname)" == "Darwin" ]; then
 
-  # Install Homebrew
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-  # Inatall Hombrew Cask
-  brew tap caskroom/cask
-
-  # Install development tools
-  brew install bash git wget tree
-
-  # Install development apps
-  brew cask install sublime-text docker virtualbox vagrant
-
-  # Install development fonts
-  brew cask install font-myrica
+  # Run setup_macOS.sh
+  . etc/setup_macOS.sh
 
 fi
---
+++ etc/setup_macOS.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Inatall Hombrew Cask
+brew tap caskroom/cask
+
+# Install development tools
+brew install bash git wget tree
+
+# Install development apps
+brew cask install sublime-text docker virtualbox vagrant
+
+# Install development fonts
+brew cask install font-myrica
